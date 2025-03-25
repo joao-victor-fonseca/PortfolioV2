9113587f41e07fbd34472dfa2270fbf0f45e69da
João Victor Fonseca Assis <84512746+joao-victor-fonseca@users.noreply.github.com>
2025-03-25 09:29:18 -0300

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText('joaovictorfosecaassis@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
